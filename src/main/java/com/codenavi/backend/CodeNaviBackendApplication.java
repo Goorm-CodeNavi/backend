1fb82f0ada016341e252c9cf542af1526627af52
myunbongs <myunbongs@naver.com>
2025-10-10 08:34:49 +0900

    infra: ⚙️ 스프링 초기 세팅 및 도커 파일 추가 (#2)

---
+++ src/main/java/com/codenavi/backend/CodeNaviBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codenavi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeNaviBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeNaviBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codenavi/backend/CodeNaviBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenavi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeNaviBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
